719c820882a0d36c0ae8be740dcef5079cca88e2
ManLi <lishiuman@gmail.com>
2018-04-29 20:27:37 +0100

    Update Character Movement

---
+++ Assets/Scripts/Stage/StageCurtainSwitch.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCurtainSwitch : MonoBehaviour {
+
+    Animator animator;
+    bool isOpened = false;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+        if(Input.GetKeyDown(KeyCode.O)){
+            animator.Play(isOpened ? "CloseCurtain" : "OpenCurtain");
+            isOpened = !isOpened;
+        }	
+	}
+
+    /*
+	IEnumerator CurtainProgress(bool isOpen){
+        isOpened = isOpen;
+        StopCoroutine("TurnCurtainOpenClose");
+        StopCoroutine("TurnCurtainOpenClose");
+        StartCoroutine(TurnCurtainOpenClose(isOpen, true));
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine(TurnCurtainOpenClose(isOpen, false));
+    }
+
+    IEnumerator TurnCurtainOpenClose(bool isOpen, bool isLeft)
+    {
+        if (isLeft)
+        {
+            if (isOpen)
+            {
+                while (curL.transform.position.x >= posL.x - 16)
+                {
+
+                    curL.transform.position -= new Vector3(0.1f, 0, 0);
+                    yield return new WaitForSeconds(0.01f);
+                }
+            }
+            else
+            {
+                while (curL.transform.position.x <= posL.x)
+                {
+
+                    curL.transform.position += new Vector3(0.1f, 0, 0);
+                    yield return new WaitForSeconds(0.01f);
+                }
+            }
+        }
+        else
+        {
+            if (isOpen)
+            {
+                while (curR.transform.position.x <= posR.x + 16)
+                {
+                    curR.transform.position += new Vector3(0.1f, 0, 0);
+                    yield return new WaitForSeconds(0.01f);
+                }
+            }else{
+                while (curR.transform.position.x <= posR.x)
+                {
+                    curR.transform.position -= new Vector3(0.1f, 0, 0);
+                    yield return new WaitForSeconds(0.01f);
+                }
+            }
+        }
+    }
+
+    void getStageCurtain(){
+        string n = transform.GetChild(0).name;
+        if(n[n.Length-1].CompareTo('L') == 0)
+        {
+            curL = transform.GetChild(0).gameObject;
+            curR = transform.GetChild(1).gameObject;
+        }
+        else
+        {
+            curL = transform.GetChild(1).gameObject;
+            curR = transform.GetChild(0).gameObject;
+        }
+    }
+    */
+}
--- Assets/Scripts/Stage/TestAction.cs
+++ Assets/Scripts/Stage/TestAction.cs
@@ -4,42 +4,114 @@ using UnityEngine;
 
 public class TestAction : MonoBehaviour {
     Animator animator;
+    bool actionFinished = false;
+    bool isInOut = false;
+    int nextActionNum = 0;
+    Vector3 defaultPosY;
+
     [SerializeField]
     string[] clipNames;
-    bool actionFinished = false;
-    int nextActionNum = 0;
-    int[] ActionNumArray = {1,2,1,2,1,2};
+    [SerializeField]
+    float[] afterClipDelayArray;
+    [SerializeField]
+    int[] actionNumArray;
+    [SerializeField]
+    float[] eachActionDelayArray;
+    [SerializeField]
+    Vector3[] offsetArray;
+    [SerializeField]
+    Transform[] offsetMarkerArray;
+    [SerializeField]
+    bool[] isFixedPos;
+    [SerializeField]
+    bool[] applyClipDelay;
+    [SerializeField]
+    GameObject smoke;
 
 	void Start () {
+        defaultPosY = new Vector3(0,transform.position.y,0);
         animator = GetComponent<Animator>();
-        StartCoroutine(SetInOut(true));
+        SetInOut(true);
         //StartCoroutine(changeAction(clipNames[2], new Vector3(1,0,0), 0.2f));
+        Initialize();
 
 	}
 
+    void Initialize(){
+        if(offsetMarkerArray == null){
+            offsetMarkerArray = new Transform[actionNumArray.Length];
+        }
+        if (offsetMarkerArray == null)
+        {
+            offsetMarkerArray = new Transform[actionNumArray.Length];
+        }
+        if (isFixedPos == null)
+        {
+            isFixedPos = new bool[actionNumArray.Length];
+        }
+        if (offsetArray == null)
+        {
+            offsetArray = new Vector3[actionNumArray.Length];
+        }
+        if (eachActionDelayArray == null)
+        {
+            eachActionDelayArray = new float[actionNumArray.Length];
+        }
+        if(applyClipDelay == null){
+            applyClipDelay = new bool[actionNumArray.Length];
+        }
+        printTotalActionTime();
+    }
+
 	private void Update()
 	{
-        if(actionFinished){
+        if (actionFinished)
+        {
             actionFinished = false;
             if (nextActionNum != -1)
             {
-                StartCoroutine(ChangeAction(clipNames[ActionNumArray[nextActionNum]], new Vector3(1, 0, 0), 0.2f));
-                if(nextActionNum >= ActionNumArray.Length-1){
+                float delayTime = -1; // -1 equals to pause action
+                if(eachActionDelayArray[nextActionNum] >= 0){
+                    delayTime = eachActionDelayArray[nextActionNum];
+                }else if(applyClipDelay[nextActionNum]){
+                    if (afterClipDelayArray[actionNumArray[nextActionNum]] >= 0)
+                    {
+                        delayTime = afterClipDelayArray[actionNumArray[nextActionNum]];
+                    }
+                }
+                ChangeAction(clipNames[actionNumArray[nextActionNum]], getOffsetPosition(nextActionNum), delayTime,isFixedPos[nextActionNum]);
+                if (nextActionNum >= actionNumArray.Length - 1)
+                {
                     nextActionNum = -1;
-                }else{
+                }
+                else
+                {
                     nextActionNum++;
                 }
-            }else{
-                StartCoroutine(SetInOut(false));
+            }
+            else
+            {
+                SetInOut(false);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                    NextAction();
             }
         }
 	}
 
 
-	IEnumerator SetInOut(bool trueFalse){
+	IEnumerator SetInOutThread(bool trueFalse){
         int num = 50;
-        if (trueFalse)
+
+        if (trueFalse && !isInOut)
         {
+            isInOut = true;
+            Instantiate(smoke,transform.position-Vector3.up,Quaternion.identity);
+            setEnabledRenderers(trueFalse);
             for (int i = 0; i < num; i++)
             {
                 transform.Rotate(new Vector3(0, i, 0));
@@ -50,30 +122,55 @@ public class TestAction : MonoBehaviour {
                 transform.Rotate(new Vector3(0, i, 0));
                 yield return new WaitForSeconds(0.01f);
             }
-        }else{
+        }else if (!trueFalse && isInOut ){
+            isInOut = false;
             for (int i = 0; i < num; i++)
             {
                 transform.Rotate(new Vector3(0, i, 0));
                 yield return new WaitForSeconds(0.01f);
             }
+            setEnabledRenderers(trueFalse);
+            Instantiate(smoke, transform.position- Vector3.up, Quaternion.identity);
         }
-        setEnabledRenderers(trueFalse);
+
         transform.rotation = Quaternion.Euler(0,0,0);
         yield return new WaitForSeconds(0.2f);
         actionFinished = true;
     }
 
-    IEnumerator ChangeAction(string clipName, Vector3 offset, float secToNextAction){
-        for (int i = 0; i < 180; i+=10){
-            transform.Rotate(0, 20, 0);
-            if(i == 140){
+    IEnumerator ChangeActionThread(string clipName, Vector3 offset, float secToNextAction, bool offsetToFixedPos = false){
+        for (int i = 0; i < 90; i+=5){
+            transform.Rotate(0, 10, 0);
+            if(i == 45){
+                transform.rotation = Quaternion.Euler(0, -90, 0);
                 animator.Play(clipName);
-                transform.position += offset;
+                if (!offsetToFixedPos)
+                {
+                    transform.position += offset;
+                }else{
+                    transform.position = offset;
+                }
             }
-            yield return new WaitForSeconds(0.02f);
+            yield return new WaitForSeconds(0.01f);
         }
         transform.rotation = Quaternion.Euler(0, 0, 0);
-        yield return new WaitForSeconds(secToNextAction);
+        if (secToNextAction >=0 )
+        {
+            yield return new WaitForSeconds(secToNextAction);
+            actionFinished = true;
+        }
+    }
+
+    public void ChangeAction(string clipName, Vector3 offset, float secToNextAction, bool offsetToFixedPos = false){
+        StartCoroutine(ChangeActionThread(clipName, offset, secToNextAction, offsetToFixedPos));
+    }
+
+    public void SetInOut(bool trueFalse){
+        StartCoroutine(SetInOutThread(trueFalse));
+    }
+
+    public void NextAction(){
+        StopCoroutine("ChangeActionThread");
         actionFinished = true;
     }
 
@@ -81,12 +178,60 @@ public class TestAction : MonoBehaviour {
     void setEnabledRenderers(bool e){
         SpriteRenderer[] renderers;
         renderers = GetComponentsInChildren<SpriteRenderer>();
-        Debug.Log(renderers.Length);
+        //Debug.Log(renderers.Length);
         foreach (SpriteRenderer r in renderers)
         {
             r.enabled = e;
         }
     }
 
+    Vector3 getOffsetPosition(int num){
+        if (num < offsetMarkerArray.Length)
+        {
+            if (offsetMarkerArray[num] != null)
+            {
+                return offsetMarkerArray[num].position;
+            }else{
+                if (num < offsetArray.Length)
+                {
+                    return offsetArray[num];
+                }
+                else
+                {
+                    Debug.LogError("offsetArray out of index");
+                }
+            }
+        }
+        else
+        {
+            //Debug.LogError("offsetMarkerArray out of index");
+            if (num < offsetArray.Length)
+            {
+                return offsetArray[num];
+            }
+            else
+            {
+                Debug.LogError("offsetArray out of index");
+            }
+        }
+       
+        return Vector3.zero;
+    }
+
+    void printTotalActionTime(){
+        float sum = 0;
+        for (int i = 0; i < actionNumArray.Length; i++){
+            if(eachActionDelayArray[i] >= 0f){
+                sum += eachActionDelayArray[i];
+            }else if(applyClipDelay[i]){
+                if(afterClipDelayArray[actionNumArray[i]] > 0f){
+                    sum += afterClipDelayArray[actionNumArray[i]];
+                }
+            }
+        }
+        sum += 2; // in and out animation Time
+        sum += 0.68f * actionNumArray.Length; // total change anim time
+        Debug.Log("TotalAnimTime("+gameObject.name+"): " + sum+" sec");
+    }
 
 }
